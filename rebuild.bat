07d43c8a34475d6947c66f642ff8f796db509223
Boris Scheiman <bscheiman@gmail.com>
2015-11-18 12:41:45 -0600

    NuGet tools

---
+++ rebuild.bat
@@ -0,0 +1,22 @@
+@echo off
+SET FILE=EnlaceFiscal
+echo WScript.Echo DateDiff("s", "01/01/1970 00:00:00", Now()) > epoch.vbs
+for /f "delims=" %%x in ('cscript /nologo epoch.vbs') do set epoch=%%x
+del epoch.vbs
+SET FULLVERSION=1.1.%epoch%
+
+tools\nuget.exe restore EnlaceFiscal.sln -OutputDirectory %cd%\packages -NonInteractive
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Clean
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild /p:Configuration=Release
+
+del D:\Desktop\NuGet\%FILE%*.nupkg
+tools\nuget.exe pack EnlaceFiscal.nuspec -p Configuration=Release -Version %FULLVERSION%
+
+move *.nupkg D:\Desktop\NuGet\
+
+IF "%1" == "push" GOTO PUSH
+GOTO END
+:PUSH
+tools\nuget push D:\Desktop\NuGet\%FILE%.%FULLVERSION%.nupkg
+:END
